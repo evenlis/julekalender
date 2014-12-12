fc5b8d40e1b49b4bf28949ecfd691eac3c3d0893
Even Lislebø <e.lislebo@gmail.com>
2014-12-12 07:36:30 +0100

    12, haskell

---
+++ 12.hs
@@ -0,0 +1,5 @@
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
+
+main :: IO ()
+main = print $ length [ 1 | y<-[1337..2014], m<-[1..12], ((==5) . day . toWeekDate) $ fromGregorian y m 13] where day (_,_,a) = a
